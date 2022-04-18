4256ba2c93ed664b44f18a4978d6ee3115d9ab48
Vladimir <bigcorvet@gmail.com>
2022-04-18 17:35:41 +0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, ChangeEvent, useState } from "react";
+import {ITask} from './Interfaces'
+import { TodoTask } from "./components/TodoTask";
+import "./App.css";
 
-function App() {
+const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    } else {
+      setDeadline(Number(event.target.value));
+    }
+  };
+
+  const addTask = (): void => {
+    const newTask = {taskName: task, deadline: deadline}
+    setTodoList([...todoList, newTask])
+    setTask('')
+    setDeadline(0)
+  }
+
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodoList(todoList.filter((task) => {
+      return task.taskName !== taskNameToDelete
+    }))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="inputContainer">
+          <input
+            type="text"
+            placeholder="Задача..."
+            name="task"
+            value={task}
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            placeholder="Время выполнения (в днях)..."
+            name="deadline"
+            value={deadline}
+            onChange={handleChange}
+          />
+        </div>
+        <button onClick={addTask}>Добавить задачу</button>
+      </div>
+      <div className="todoList">
+        {
+          todoList.map((task: ITask, index: number) => (
+            <TodoTask key={index} task={task} completeTask={completeTask} />
+          ))
+        }
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/TodoTask.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { ITask } from '../Interfaces'
+
+interface Props {
+    task: ITask
+    completeTask(taskNameToDelete: string): void 
+}
+
+export const TodoTask = ({task, completeTask}: Props) => {
+  return (
+    <div className='task'>
+       <div className="content">
+        <span>{task.taskName}</span>
+        {' '}
+        <span>{task.deadline}</span>
+       </div>
+       <button onClick={() => completeTask(task.taskName)}>X</button>
+    </div>
+  )
+}
